dd8548b7b35525f953e3f93d61233f25c7b997dc
asv-soft-u2 <u02@asv.me>
2023-07-13 15:54:41 +0300

    fix: add WeatherActionView for displaying weather data
    This commit adds new functionality for displaying weather data on the map via the WeatherActionView feature. The WeatherActionViewModel was also created to handle actions such as updating weather information based on geographical location. These changes were necessary to extend the drone application's capability to include weather data visualization, thus improving flight planning and safety.

---
+++ src/Asv.Drones.Gui.Weather/Shell/Pages/Map/Actions/WeatherActionView.axaml.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Asv.Drones.Gui.Core;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace Asv.Drones.Gui.Weather;
+
+[ExportView(typeof(WeatherActionViewModel))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public partial class WeatherActionView : ReactiveUserControl<WeatherActionViewModel>
+{
+    public WeatherActionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ src/Asv.Drones.Gui.Weather/Shell/Pages/Map/Actions/WeatherActionViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Asv.Common;
+using Asv.Drones.Gui.Core;
+using Avalonia.Controls;
+using DynamicData.Binding;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Asv.Drones.Gui.Weather;
+
+[Export(FlightPageViewModel.UriString,typeof(IMapAction))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public class WeatherActionViewModel : ViewModelBase, IMapAction
+{
+    private readonly IWeatherService _weatherService;
+    private readonly ILocalizationService _localizationService;
+
+    public WeatherActionViewModel() : base("asv:shell.page.map.action.weather")
+    {
+        if (Design.IsDesignMode)
+        {
+            Visibility = true;
+            
+            CurrentWeatherData = new WeatherData
+            {
+                WindSpeed = 5,
+                WindDirection = 123,
+                Temperature = 295.25
+            };
+
+            WindDirection = 20;
+            WindSpeedString = $"{CurrentWeatherData.WindSpeed} m/s";
+            Temperature = $"{CurrentWeatherData.Temperature} K";
+        }
+    }
+    
+    [ImportingConstructor]
+    public WeatherActionViewModel(IWeatherService weatherService, ILocalizationService localizationService) : this()
+    {
+        _weatherService = weatherService;
+        _localizationService = localizationService;
+
+        _weatherService.LastWeatherData
+            .Subscribe(_ => CurrentWeatherData = _)
+            .DisposeItWith(Disposable);
+        
+        _weatherService.Visibility
+            .Subscribe(_ => Visibility = _)
+            .DisposeItWith(Disposable);
+    }
+    
+    public ICommand UpdateWeather { get; set; }
+    
+    [Reactive]
+    public WeatherData CurrentWeatherData { get; set; }
+    
+    [Reactive]
+    public string WindSpeedString { get; set; }
+    
+    [Reactive]
+    public double WindSpeed { get; set; }
+
+    [Reactive]
+    public double WindDirection { get; set; }
+    
+    [Reactive]
+    public string Temperature { get; set; }
+
+    [Reactive]
+    public bool Visibility { get; set; }
+    
+    public async Task UpdateWeatherImpl(GeoPoint location)
+    {
+        CurrentWeatherData = await _weatherService.GetWeatherData(location);
+    }
+
+    public int Order => 3;
+    public IMapAction Init(IMap context)
+    {
+        UpdateWeather = ReactiveCommand.CreateFromTask(
+            () => UpdateWeatherImpl(context.Center)).DisposeItWith(Disposable);
+        
+        this.WhenPropertyChanged(_ => _.CurrentWeatherData)
+            .Subscribe(_ =>
+            {
+                if (CurrentWeatherData != null)
+                {
+                    WindDirection = CurrentWeatherData.WindDirection;
+                    WindSpeed = CurrentWeatherData.WindSpeed;
+                    WindSpeedString = _localizationService.Velocity.FromSiToStringWithUnits(CurrentWeatherData.WindSpeed);
+                    Temperature = _localizationService.Temperature.FromSiToStringWithUnits(CurrentWeatherData.Temperature);
+
+                    _weatherService.LastWeatherData.OnNext(CurrentWeatherData);
+                }
+            })
+            .DisposeItWith(Disposable);
+        
+        return this;
+    }
+}
